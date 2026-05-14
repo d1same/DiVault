8f7cf340d220ff5a24c8b610f594d4df3bdf2c5a
Moe Mobasheri <moe@mobasheri.us>
2026-05-13 20:34:10 -0400

    Release v0.1.10

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'divault-v107';
-const ASSETS = ['/', '/app.html', '/styles.css?v=107', '/app.js?v=107', '/manifest.webmanifest', '/assets/icon.svg'];
+const CACHE = 'divault-v108';
+const ASSETS = ['/', '/app.html', '/styles.css?v=108', '/app.js?v=108', '/manifest.webmanifest', '/assets/icon.svg'];
 const ASSET_PATHS = new Set(ASSETS);
 
 function cacheableRequest(request) {
