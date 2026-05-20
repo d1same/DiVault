c39b34b18b065318c7341318a333dbb645a029bb
Moe Mobasheri <moe@mobasheri.us>
2026-05-20 12:36:00 -0400

    Fix mobile full-width layouts

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'divault-v208';
-const ASSETS = ['/', '/app.html', '/styles.css?v=208', '/app.js?v=208', '/manifest.webmanifest', '/assets/divault-logo.svg', '/assets/icon.svg'];
+const CACHE = 'divault-v209';
+const ASSETS = ['/', '/app.html', '/styles.css?v=209', '/app.js?v=209', '/manifest.webmanifest', '/assets/divault-logo.svg', '/assets/icon.svg'];
 const ASSET_PATHS = new Set(ASSETS.map(asset => new URL(asset, self.location.origin).pathname));
 
 function cacheableRequest(request) {
